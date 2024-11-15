c85e686fa9cbe065d92e71d17deee2efa2e28542
nele <technelegies@gmail.com>
2024-11-15 14:19:18 +0100

    Using service to store state

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,30 +1,25 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MemberService } from '../../_services/member.service';
-import { CommonModule } from '@angular/common';
-import { Member } from '../../_models/member';
-import { MemberCardComponent } from "../member-card/member-card.component";
+import { MemberCardComponent } from '../member-card/member-card.component';
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [CommonModule, MemberCardComponent],
+  imports: [MemberCardComponent],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css',
 })
-
 export class MemberListComponent implements OnInit {
-  private memberService = inject(MemberService);
-  members: Member[] = [];
+  memberService = inject(MemberService);
 
   ngOnInit(): void {
-    this.loadMembers();
+    if (this.memberService.members().length === 0) {
+      this.loadMembers();
+    }
   }
 
   loadMembers() {
-    this.memberService.getMembers().subscribe({
-      next: (members) => this.members = members,
-      error: (err) => console.error('Error fetching users', err)
-    });
+    this.memberService.getMembers();
   }
 
   // getMainPhotoUrl(user: User): string {
