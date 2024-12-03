445c34ac6a03ff57438fb337db6e99ad0ff46e5b
Nebojsa Kovacic <Nele25@live.com>
2024-12-03 21:30:08 +0100

    Adding likes feature backend part

---
+++ Hydra.WebApi/Controllers/LikesController.cs
@@ -0,0 +1,53 @@
+﻿using Hydra.WebApi.DTOs;
+using Hydra.WebApi.Entities;
+using Hydra.WebApi.Extensions;
+using Hydra.WebApi.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hydra.WebApi.Controllers
+{
+    public class LikesController(ILikesRepository likesRepository) : BasicApiController
+    {
+        [HttpPost("{targetUserId:int}")]
+        public async Task<ActionResult> AddLike(int targetUserId)
+        {
+            var sourceUserId = User.GetUserId();
+
+            if (sourceUserId == targetUserId) return BadRequest("You cannot like yourself");
+
+            var existingLike = await likesRepository.GetUserLike(sourceUserId, targetUserId);
+            if (existingLike == null)
+            {
+                var like = new UserLike
+                {
+                    SourceUserId = sourceUserId,
+                    TargetUserId = targetUserId
+                };
+
+                likesRepository.AddLike(like);
+            }
+            else
+            {
+                likesRepository.DeleteLike(existingLike);
+            }
+
+            if (await likesRepository.SaveChanges())
+                return Ok();
+
+            return BadRequest("Failed to like user");
+        }
+
+        [HttpGet("list")]
+        public async Task<ActionResult<IEnumerable<int>>> GetCurrentUserLikeIds()
+        {
+            return Ok(await likesRepository.GetCurrentUserLikeIds(User.GetUserId()));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUserLikes(string predicate)
+        {
+            var users = await likesRepository.GetUserLikes(predicate, User.GetUserId());
+            return Ok(users);  
+        }
+    }
+}
--- Hydra.WebApi/Data/DataContext.cs
+++ Hydra.WebApi/Data/DataContext.cs
@@ -3,11 +3,28 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Hydra.WebApi.Data
 {
-    public class DataContext : DbContext
+    public class DataContext(DbContextOptions options) : DbContext(options)
     {
-        public DataContext(DbContextOptions options) : base(options)
-        {
-        }
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserLike>().HasKey(k=>new { k.SourceUserId, k.TargetUserId});
+
+            builder.Entity<UserLike>()
+                .HasOne(s=>s.SourceUser)
+                .WithMany(l=>l.LikedUsers)
+                .HasForeignKey(s=>s.SourceUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<UserLike>()
+            .HasOne(s => s.TargetUser)
+            .WithMany(l => l.LikedByUsers)
+            .HasForeignKey(s => s.TargetUserId)
+            .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
